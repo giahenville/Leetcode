bb8095e576618259be23bc65a7e951fa10a38de4
Gia Henville <140769584+giabui@users.noreply.github.com>
2025-04-17 17:55:45 -0400

    EncodeDecodeStrings

---
+++ Arrays & Hashing/EncodeDecodeStrings.py
@@ -0,0 +1,41 @@
+from typing import List
+
+class EncodeDecode:
+    def encode(self, strs: List[str]) -> str:
+        result = ""
+
+        for word in strs:
+            result += str(len(word)) + '#' + word
+        return result
+
+    def decode(self, encodedStr: str) -> List[str]:
+        result = []
+        i = 0
+
+        while i < len(encodedStr) - 1:
+
+            numOfChars = ''
+            # find num of characters in word
+            while encodedStr[i] != '#':
+                numOfChars += encodedStr[i]
+                i += 1
+            i += 1
+
+            # create word
+            numOfChars = int(numOfChars)
+            word = encodedStr[i: i + numOfChars]
+            
+            # add word to list
+            i += numOfChars
+            result.append(word)
+ 
+        return result
+
+list1 = ["neet","code","love","you"]
+
+encodeDecodeStr = EncodeDecode()
+encoded = encodeDecodeStr.encode(list1)
+decoded = encodeDecodeStr.decode(encoded)
+
+print("Encoded str: ", encoded) # returns encoded string
+print("Decoded string: ", decoded) # returns decoded string
